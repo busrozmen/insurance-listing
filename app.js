2907ec470b2590714e85a94debbe81d3dd277637
Busra Ozmen <busra.ozmen@akinon.com>
2021-05-13 13:41:43 +0300

    css improvements and fetch error added

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 $(document).ready(function () {
 	fetch('./listing-data.json')
-	.then((response) => {
+		.then((response) => {
 			return response.json();
-	})
-	.then((data) => {
-		data=data.offerList;
-		data.map((list) => {
-			renderLists(list);
-		});
-	});
-
-    function renderLists(list) {
-				$('.list-wrapper').append('<div class="list">'+
-				'<div class="img-wrapper"><img src='+list.ImagePath+' /></div>'+
-				'<div class="description-wrapper">'+
-					'<div class="product-desc">'+list.ProductDesc+'</div>'+
-					'<div class="firm-name">'+list.FirmName+
-					(list.popoverContent ? 
-						'<i class="fa fa-question-circle-o"></i>'+
-						'<div class="popover">'+list.popoverContent.map((item)=>(
-							'<div class="title">'+item.Title+'</div>'+
-							'<div>'+item.Description+'</div>'
-							))+'</div>' : '')+
-					'</div>'+	
-				'</div>'+
-				'<div class="price-wrapper">'+
-				(list.QuotaInfo.HasDiscount ? '<div class="old-price">Peşin <span>'+list.Cash.toLocaleString('tr-TR')+' TL</span></div>' :'')+
-				'<div class="current-price">'+(list.QuotaInfo.HasDiscount ? list.QuotaInfo.PremiumWithDiscount.toLocaleString('tr-TR') : list.Cash.toLocaleString('tr-TR'))+' TL</div>'+
-				'<div class="buy-wrapper">'+(list.SaleClosed ? '<button class="inline">TELEFONDA SATIN AL <a href="tel:444 24 00">444 24 00</a></button>' : '<button>SATIN AL</button>')+'</div>'+
-				'</div></div>'
-				)
-    }
+		})
+		.then((data) => {
+			list=data.offerList;
+			list.map((item) => {
+				renderLists(item);
+			});
+		})
+		.catch( error => {
+			console.log('Error:', error);
+		})
+	
+	function renderLists(item) {
+		$('.list-wrapper').append('<div class="list">'+
+		'<div class="img-wrapper"><img src='+item.ImagePath+' /></div>'+
+		'<div class="description-wrapper">'+
+			'<div class="product-desc">'+item.ProductDesc+'</div>'+
+			'<div class="firm-name">'+item.FirmName+
+			(item.popoverContent ? 
+				'<i class="fa fa-question-circle-o"></i>'+
+				'<div class="popover">'+item.popoverContent.map((popItem)=>(
+					'<div class="title">'+popItem.Title+'</div>'+
+					'<div>'+popItem.Description+'</div>'
+					))+'</div>' : '')+
+			'</div>'+	
+		'</div>'+
+		'<div class="price-wrapper">'+
+		(item.QuotaInfo.HasDiscount ? '<div class="old-price">Peşin <span>'+item.Cash.toLocaleString('tr-TR')+' TL</span></div>' :'')+
+		'<div class="current-price">'+(item.QuotaInfo.HasDiscount ? item.QuotaInfo.PremiumWithDiscount.toLocaleString('tr-TR') : item.Cash.toLocaleString('tr-TR'))+' TL</div>'+
+		'<div class="buy-wrapper">'+(item.SaleClosed ? '<button class="inline">TELEFONDA SATIN AL <a href="tel:444 24 00">444 24 00</a></button>' : '<button>SATIN AL</button>')+'</div>'+
+		'</div></div>'
+		)
+	}
 });
